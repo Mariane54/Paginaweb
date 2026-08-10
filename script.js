3d221372878ad1af2e90ced0ea683808c8c36f92
Mariane <137068595+Mariane54@users.noreply.github.com>
2026-08-10 19:16:14 -0300

    teste

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const botao = document.querySelector("button");
 botao.addEventlistener("click",botaoclicado);
 function botaoclicado(){
     console.log("Fui clicado");
-    let text = botao.querySelector("span");
+    let texto = botao.querySelector("span");
     texto.textContent++;
 }
